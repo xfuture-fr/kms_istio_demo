d7ef862f9e59500c91cd9e1d37b9ba1836031bc1
Feng QIN <xfuture.fr@gmail.com>
2025-10-31 00:38:29 +0100

    change to gke std

--- infra/gke.tf
+++ infra/gke.tf
@@ -1,26 +1,45 @@
-resource "google_container_cluster" "gke_autopilot" {
-  name     = "kms-istio-demo-autopilot"
-  location = var.region
+# gke.tf
+resource "google_container_cluster" "gke_standard" {
+  name     = "kms-istio-demo-std"
+  location = "europe-west9-a"          # <-- ZONE (pas la région)
 
-  enable_autopilot = true
+  remove_default_node_pool = true
+  initial_node_count       = 1
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
 
   ip_allocation_policy {
-    cluster_secondary_range_name  = "pods"
-    services_secondary_range_name = "services"
+    cluster_secondary_range_name  = google_compute_subnetwork.subnet.secondary_ip_range[0].range_name
+    services_secondary_range_name = google_compute_subnetwork.subnet.secondary_ip_range[1].range_name
   }
 
   release_channel { channel = "REGULAR" }
 
-  # Enable Workload Identity (recommended for CI/CD & least privilege)
   workload_identity_config {
     workload_pool = "${var.project_id}.svc.id.goog"
   }
 
-  depends_on = [
-    google_project_service.services,
-    google_compute_subnetwork.subnet
-  ]
+  # IMPORTANT: ne mets PAS node_locations ici (sinon multi-zones)
+}
+
+resource "google_container_node_pool" "np1" {
+  name     = "np1"
+  cluster  = google_container_cluster.gke_standard.name
+  location = "europe-west9-a"          # <-- ZONE identique
+
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    disk_size_gb = 20                  # <-- petit disque
+    disk_type    = "pd-standard"       # <-- évite le quota SSD_TOTAL_GB
+    image_type   = "COS_CONTAINERD"
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  management { 
+    auto_upgrade = true
+    auto_repair = true 
+  }
 }
